b6ebd5061a356cbf9bdd114a927a61b3552355f2
Abhinav Gupta <gupta380189@gmail.com>
2024-08-26 18:57:30 +0530

    slide show to home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
-
+import {MarqueeDemo} from "@/components/homeScreenSlideShow"
 export default function Home() {
   return (
-    <div className="min-h-screen">Hello</div>
+    <div className="min-h-screen">
+      <MarqueeDemo/>
+      </div>
   );
 }
