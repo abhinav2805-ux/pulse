d9a9e6637a2104e6fa8f1ad6ea8309150c5ea0f8
Ansh Jain <anshjain9159@gmail.com>
2024-08-26 12:21:39 +0530

    feat: added notFound

---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <div className='min-h-screen flex flex-col justify-center items-center align-middle space-y-8'>
+      <h2 className='text-4xl font-bold text-red-600'>Not Found</h2>
+      <p className='text-2xl font-semibold text-black'>Could not find requested resource</p>
+      <Link href="/">
+        <Button variant={'outline'}>
+            <span className='mr-2 text-2xl font-semibold'>Go Back</span>
+        </Button>
+      </Link>
+    </div>
+  )
+}
