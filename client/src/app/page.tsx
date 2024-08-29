0a38eabb5222d7efd382cadb548b05e162a2b4a6
Ansh Jain <anshjain9159@gmail.com>
2024-08-29 11:06:32 +0530

    feat:  added faq

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Features from "@/components/Features";
 import Image from "next/image";
 import {MarqueeDemo} from "@/components/homeScreenSlideShow"
+import FAQ from "@/components/FAQ";
 
 export default function Home() {
   return (
     <div className="min-h-screen py-16 flex flex-col px-8 rounded-xl">
       <MarqueeDemo/>
       <Features/>
+      <FAQ/>  
     </div>
   );
 }
